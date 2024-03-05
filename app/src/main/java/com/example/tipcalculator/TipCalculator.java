114811fb166903f5b14662d361e3b6c07fb97a31
ngcdan <linuss1908@gmail.com>
2024-03-05 11:37:04 +0700

    add update model, controler app

---
+++ app/src/main/java/com/example/tipcalculator/TipCalculator.java
@@ -0,0 +1,35 @@
+package com.example.tipcalculator;
+
+public class TipCalculator {
+    private float tip;
+    private float bill;
+
+    public TipCalculator(float newTip, float newBill) {
+        this.tip = newTip;
+        this.bill = newBill;
+    }
+
+    public float getTip() {
+        return tip;
+    }
+
+    public float getBill() {
+        return bill;
+    }
+
+    public void setTip(float newTip) {
+        if (newTip > 0) tip = newTip;
+    }
+
+    public void setBill(float newBill) {
+        if (newBill > 0) bill = newBill;
+    }
+
+    public float tipAmount() {
+        return bill * tip;
+    }
+
+    public float totalAmount() {
+        return bill + tipAmount();
+    }
+}
